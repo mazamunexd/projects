7bc15b69fd7f7b7e9e4f9ee69581b2404224178a
mazamunexd <arvinedseltulabing@gmail.com>
2025-04-04 15:13:17 +0800

    projects needed to fixed

---
+++ Loading Screen Project/script.js
@@ -0,0 +1,26 @@
+let images = [
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg"
+];
+
+let currentImageIndex = 0;
+let loadingImage = document.getElementById("loading-image");
+
+function showImage() {
+    loadingImage.src = images[currentImageIndex];
+    loadingImage.style.display = "block";
+    loadingImage.style.opacity = 0;
+    setTimeout(() => {
+        loadingImage.style.opacity = 1;
+    }, 0);
+    setTimeout(() => {
+        loadingImage.style.opacity = 0;
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        showImage();
+    }, 4000);
+}
+
+showImage();
